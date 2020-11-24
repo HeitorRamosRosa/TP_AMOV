e130f3260a870c623c933b95fb3d6bb6ac4b79f0
TehJudas <diogo.lameiro@hotmail.com>
2020-11-24 19:58:32 +0000

    Translations and misc
    poucas coisas

--- app/src/main/java/pt/isec/supraindustries/tp_amov/oldfiles/FirstFragment.kt
+++ app/src/main/java/pt/isec/supraindustries/tp_amov/oldfiles/FirstFragment.kt
@@ -1,4 +1,4 @@
-package pt.isec.supraindustries.tp_amov
+package pt.isec.supraindustries.tp_amov.oldfiles
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import pt.isec.supraindustries.tp_amov.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
