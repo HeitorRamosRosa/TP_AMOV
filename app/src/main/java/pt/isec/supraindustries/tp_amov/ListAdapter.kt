e3a974b1489433b58561dcd037bf48f6656ce0d9
HeitorRamosRosa <58524894+HeitorRamosRosa@users.noreply.github.com>
2020-11-29 16:11:32 +0000

    MostrarListasWorking

--- app/src/main/java/pt/isec/supraindustries/tp_amov/ListAdapter.kt
+++ app/src/main/java/pt/isec/supraindustries/tp_amov/ListAdapter.kt
@@ -1,16 +1,28 @@
 package pt.isec.supraindustries.tp_amov
 
 import android.view.LayoutInflater
+import android.view.OrientationEventListener
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pt.isec.supraindustries.tp_amov.Data.Lista
 
-class ListAdapter(private val listList: ArrayList<Lista>): RecyclerView.Adapter<ListAdapter.UserViewHolder>(){
+class ListAdapter(private val listList: ArrayList<Lista>,private val listener: OnItemClickListener): RecyclerView.Adapter<ListAdapter.UserViewHolder>(){
 
-    class UserViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    inner class UserViewHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener {
         val username: TextView = view.findViewById(R.id.listName)
+
+        init{
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val pos : Int = adapterPosition
+            if(pos != RecyclerView.NO_POSITION){
+                listener.onItemClick(pos)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
@@ -20,9 +32,15 @@ class ListAdapter(private val listList: ArrayList<Lista>): RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         holder.username.text = listList[position].nome;
+
     }
 
     override fun getItemCount(): Int = listList.size;
 
+    interface OnItemClickListener{
+
+        fun onItemClick(pos : Int)
+    }
 }
+
 
--- app/src/main/java/pt/isec/supraindustries/tp_amov/MostrarListasActivity.kt
+++ app/src/main/java/pt/isec/supraindustries/tp_amov/MostrarListasActivity.kt
@@ -3,8 +3,10 @@ package pt.isec.supraindustries.tp_amov
 import android.icu.number.IntegerWidth
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,19 +14,20 @@ import pt.isec.supraindustries.tp_amov.Data.Lista
 import pt.isec.supraindustries.tp_amov.Data.Produto
 
 
-class MostrarListasActivity : AppCompatActivity() {
+class MostrarListasActivity : AppCompatActivity(),ListAdapter.OnItemClickListener {
 
     var produtoList: MutableList<Produto>? = null
     lateinit var removeButton: Button
     lateinit var arrayListas: ArrayList<Lista>
     lateinit var removeEt: EditText
     lateinit var r : RecyclerView
+    private val m = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostar_listas)
 
-        arrayListas = arrayListOf(Lista("Lista1"),Lista("Lista2"),Lista("Lista3"),Lista("Lista4"),Lista("Lista5"),)
+        arrayListas = arrayListOf(Lista("Lista0"),Lista("Lista1"),Lista("Lista2"),Lista("Lista3"),Lista("Lista4"))
 
         r = findViewById(R.id.itemList)
         removeButton = findViewById(R.id.removeButton)
@@ -33,16 +36,29 @@ class MostrarListasActivity : AppCompatActivity() {
 
         r.apply {
             layoutManager = LinearLayoutManager(this@MostrarListasActivity)
-            adapter = ListAdapter(arrayListas)
+            adapter = ListAdapter(arrayListas,m)
         }
     }
 
     fun onRemoveButtonClicked(view: View) {
-        var pos: Int = Integer.parseInt(removeEt.text.toString())
-        arrayListas.removeAt(pos)
-        r.apply {
-            adapter?.notifyDataSetChanged()
+        if(!removeEt.text.toString().isEmpty()){
+            var pos: Int = Integer.parseInt(removeEt.text.toString())
+            if(pos < arrayListas.size && pos > -1){
+                arrayListas.removeAt(pos)
+                r.apply {
+                    adapter?.notifyItemRemoved(pos)
+                }
+            }
         }
 
+    }
+    
+
+    override fun onItemClick(pos: Int) {
+        Toast.makeText(this, "to new screen $pos", Toast.LENGTH_SHORT).show()
+        val clickedItem : Lista = arrayListas[pos]
+        r.apply {
+            adapter?.notifyItemChanged(pos)
+        }
     }
 }
